7c82026b7e16b7c0159243eee277a3e5a3e155ee
YitongHe <yitonghe@usc.edu>
2018-08-14 05:23:29 +0000

    Set database url env value

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var commentRoutes = require("./routes/comments"),
     indexRoutes = require("./routes/index");
 
 //mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
-mongoose.connect("mongodb://yitonghe:bohebohe5@ds119692.mlab.com:19692/yitonghe_webdev", {useNewUrlParser: true});
+mongoose.connect(DATABASEURL, {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
