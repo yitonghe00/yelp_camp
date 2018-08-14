8cf562b98e3fd534d9961566d7112664a001ab9a
YitongHe <yitonghe@usc.edu>
2018-08-14 06:49:45 +0000

    Set min width for sign up form

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ var commentRoutes = require("./routes/comments"),
 
 var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp";
 mongoose.connect(url, {useNewUrlParser: true});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
